e50b2333e66b293a7da206aa26631d54e3caff5d
Paul De Cannière <paul@allusion.be>
2026-06-30 17:25:51 +0200

    pipeline: staging deploy on dev + real satellite IDs

--- juno.config.ts
+++ juno.config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from "@junobuild/config";
 export default defineConfig({
   satellite: {
     ids: {
-      development: "<DEV_SATELLITE_ID>",
-      production: "<PROD_SATELLITE_ID>",
+      staging: "uaw2u-wqaaa-aaaal-as2cq-cai",
+      production: "uhx4a-3iaaa-aaaal-as2ca-cai",
     },
     hosting: {
       source: "dist",
       predeploy: ["npm run build"],
-    }
+    },
   },
 });
